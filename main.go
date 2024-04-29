73d540e30a9e940e608d458489674d86b7422e8d
Jordan Martin <jordan.martin2890@gmail.com>
2024-04-29 14:36:17 -0500

    implemented user creation

--- main.go
+++ main.go
@@ -1,26 +1,40 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
+	"github.com/jordanmatinwebdev/rss_blog_agregator_go/internal/database"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
+	DB *database.Queries
 }
 
 func main() {
 	godotenv.Load()
 	port := os.Getenv("PORT")
+	dbURL := os.Getenv("DB_CONNECTION_STRING")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	dbQueries := database.New(db)
 
 	mux := http.NewServeMux()
 	corsMux := middlewareCors(mux)
-	apiCfg := &apiConfig{}
+	
+	apiCfg := &apiConfig{
+		DB: dbQueries,
+	}
 
 	mux.HandleFunc("GET /v1/readiness", apiCfg.handlerReady)
 	mux.HandleFunc("GET /v1/err", apiCfg.handlerErr)
+	mux.HandleFunc("POST /v1/users", apiCfg.handlerCreateUsers)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
