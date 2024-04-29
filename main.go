ca3ac0af3809a2c5655e07822badaf3b0c5a1c35
Jordan Martin <jordan.martin2890@gmail.com>
2024-04-29 11:45:33 -0500

    init server setup

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 func main() {
-	
+	godotenv.Load()
+	port := os.Getenv("PORT")
+
+	mux := http.NewServeMux()
+	corsMux := middlewareCors(mux)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: corsMux,
+	}
+
+	log.Printf("Server running on port: %s", port)
+	log.Fatal(srv.ListenAndServe())
 }
